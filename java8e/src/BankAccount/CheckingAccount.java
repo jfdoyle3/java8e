09f5a42c1c892a62dceb9dbc2a0507e1b543d604
jfdoyle3 <indellibleink@gmail.com>
2020-10-22 21:37:24 -0400

    remove project MAIN wasn't working

--- java8e/src/BankAccount/CheckingAccount.java
+++
@@ -1,7 +0,0 @@
-package BankAccount;
-
-public class CheckingAccount extends BankAccount {
-    public CheckingAccount(int accountNum, int balance, String owner) {
-        super(accountNum, balance, owner, "Checking");
-    }
-}
