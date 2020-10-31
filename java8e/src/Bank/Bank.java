04050b3703d6ccbcc79da4977ad22d67b1ed06e1
Jim Doyle <indellibleink@gmail.com>
2020-10-30 23:30:32 -0400

    added more account types to Bank class

--- java8e/src/Bank/Bank.java
+++ java8e/src/Bank/Bank.java
@@ -1,8 +1,7 @@
 package Bank;
 
-import BankAccount.Account;
-import BankAccount.CheckingAccount;
-import BankAccount.SavingsAccount;
+import BankAccount.*;
+import BankTools.UI;
 
 import java.util.HashMap;
 
@@ -20,14 +19,53 @@ public class Bank {
         accounts = new HashMap<>();
     }
 
-    public void addAccount() {
-        // TODO: get client
-        // TODO: create account giving it the client.
-        Account newAccount = new Account();
+    public void addClient(String firstName, String lastName) {
+        clientCount += 1;
+        clients.put(clientCount.toString(), new Client(firstName, lastName, clientCount.toString()));
     }
 
-    public Account createAccount() {}
+    public void addAccount(String clientId, String accountType, int startingBal, UI ui) {
+        // TODO: get client from client collection
+        Client client = clients.get(clientId);
+        // TODO: create account giving it the client.
+        Account newAccount = createAccount(accountType, client, ++accountCount, startingBal, ui);
+        // TODO: attach to accounts collection
+        client.addAccount(newAccount);
+        accounts.put(accountCount, newAccount);
+    }
 
-    public SavingsAccount creatsSavingsAccount() {};
-    public CheckingAccount creactCheckingAccount() {};
+    public Account createAccount(String accountType, Client client, int accountId, int startingBal, UI ui) {
+        switch (accountType) {
+            case "checking":
+                return new CheckingAccount(accountId, startingBal, client);
+            case "savings":
+                int interest = ui.requestInt("Please enter interest rate:");
+                return new SavingsAccount(accountId, startingBal, client, interest);
+            case "CDInvestment" :
+                int cdInterest = ui.requestInt("Please enter CD interest rate:");
+               int period=ui.requestInt("What is the period");
+                int timeframe=ui.requestInt("What is the timeframe");
+                return new CDInvestment(startingBal,  accountId,client,cdInterest, period, timeframe);
+            case "Investment":
+                int interestRate = ui.requestInt("Please enter interest rate:");
+                String periodType=ui.requestString("Enter period: (D)ay, (W)eek, (B)iweekly, (M)onthly,(Q)uarterly, (Y)early");
+                int investPeriod=ui.requestInt("What is the period");
+                return new InvestmentAccount( startingBal, accountId, client, interestRate, investPeriod,periodType);
+            default:
+                return null;
+        }
+    }
+
+    public void displayClients() {
+        for (var client : clients.keySet()){
+            System.out.println(clients.get(client));
+        }
+    }
+
+    public Account getAccount(int accountId) {
+        return accounts.get(accountId);
+    }
+
+//    public SavingsAccount createSavingsAccount() {};
+//    public CheckingAccount createCheckingAccount() {};
 }
